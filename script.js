fe7568e894168fb2bef5d44a627d15867643bcdf
Kumar9525 <92426822+Kumar9525@users.noreply.github.com>
2024-04-16 10:32:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-let submitBtn = document.querySelector('#submit');
-let checkBox = document.querySelector('#checkbox');
-let username = document.querySelector('#username');
-let password= document.querySelector('#password');
-let loginBtn=document.querySelector('#existing');
+//your JS code here. If required.
+let formEle = document.querySelector("form");
+let userList = [];
+formEle.addEventListener("submit", submitForm);
 
-submitBtn.addEventListener('click',(event)=>{
-	event.preventDefault();
-// if(username.value === '' || password.value === ''){}
-  if(checkbox.checked){
-  let users =[];
-  let user ={
-   UserName : username.value,
-   Password : password.value}
-   users.push(user);
-   localStorage.setItem('users', JSON.stringify(users));
-  }
-  else if(JSON.parse(localStorage.getItem('users'))){
-  	let savedUser =	JSON.parse(localStorage.getItem('users'));
-    savedUser.forEach((user)=>{
-      if(user.UserName === username.value ){      
-	    loginBtn.removeAttribute('hidden')
-      loginBtn.addEventListener('click',(e)=>{
-  		  e.preventDefault();
-  		  alert(`Logged in as ${user.UserName}`)})
-      }
-    });
-  }
-  else{
-		alert(`Logged in as ${username.value}`);
-    }
-});
+function submitForm(e){
+	e.preventDefault();
+	 
+	let checkEle = document.getElementById("checkbox").checked;
+	let userName = document.getElementById("username").value;
+	let passWord = document.getElementById("password").value;
+	let userObj = {
+		username: userName,
+		password: passWord
+	};
+	if(checkEle){
+		userList.push(userObj);
+		localStorage.setItem("usersData", JSON.stringify(userList));
+	}
+	alert( `Logged in as ${userName}`);
+	formEle.reset();
+}
